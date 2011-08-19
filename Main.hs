846f61f87bf0798d29d9b80011e5a02ec5887ead
s s <smschm@umich.edu>
2011-08-19 12:46:13 -0400

    documentation

--- Main.hs
+++ Main.hs
@@ -1,30 +1,20 @@
 import qualified Data.Set as S
 import qualified Data.Map as M
 import Data.Array
-import Data.List (nub)
-import Data.Maybe (catMaybes)
 
 import Types
 import Operations
 import DotRender
 import Parser
--------- parser:
------- generals
 
-printT :: Todo -> IO ()
-printT t = do
-    putStr (show (priority t))
-    putStr ":\t"
-    putStrLn (name t)
-
+-- | Simple test: print tasks that can be done now
 doNow = do
     a <- testParser
     let di = filter (canDoNow a) (M.keys a)
-    mapM_ printT $ map (a !!!) di
+    mapM_ print $ map (a !!!) di
 
 main = do
     a <- testParser
     --doNow
     --dotRender noSubgraphDRS a
-    dotRender defaultDRS a
-    --print (contexts a)
+    --dotRender defaultDRS a
--- Types.hs
+++ Types.hs
@@ -5,23 +5,28 @@ import qualified Data.Map as M
 type Str = String
 type Ident = Str
 
+-- | Left x is not done, Right x is done; with reason/commentary x
 type Progress = Either Str Str
--- where Left is Not Done, Right is Done
 
+-- | One TODO item
 data Todo = Todo {
-    name :: Str,
-    prereqs :: S.Set Ident,
-    softprs :: S.Set Ident,
-    progress :: Progress,
-    priority :: Int,
-    wheres :: [Str],
-    context :: Maybe Str
-} deriving (Eq, Show)
+    name     :: Str,         -- ^ The name of the task, i.e. "get some milk"
+    prereqs  :: S.Set Ident, -- ^ 'Hard' prerequisites
+    softprs  :: S.Set Ident, -- ^ 'Soft' prerequisites, containing suggestions
+    progress :: Progress,    -- ^ Progress on the task
+    priority :: Int,         -- ^ An arbitrary numerical priority
+    wheres   :: [Str],       -- ^ Where is the task primarily done?
+    context  :: Maybe Str    -- ^ The context or project this task belongs to
+} deriving (Eq)
+
+instance Show Todo where
+    show t = (show (priority t)) ++ ":\t" ++ (name t)
 
 data Spec = Done Str | NotDone Str | PRef [Ident] | SPRef [Ident] | --Depends [Int] |
     Where Str | Context Str | Priority Int | Name Str
     deriving (Eq, Show)
 
+-- | The 'big paper': a map from identification keys to TODO items
 type BP = M.Map Ident Todo
 
 defaultT = Todo "" S.empty S.empty (Left "") 1 [] Nothing
